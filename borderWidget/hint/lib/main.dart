56e5cf26dda4ea1ca83cc17989737161ff8c3d8c
datbt <datb1505711@student.ctu.edu.vn>
2019-05-31 16:59:30 +0700

    add project

---
+++ borderWidget/hint/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyWidget());
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+    child: Text(
+      "text",
+      style: TextStyle(fontSize: 30.0),
+    ),
+  );
+  }
+  // This widget is the root of your application.
+}
+
---
+++ switch/android/app/src/main/java/com/example/set_column_width/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.set_column_width;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
